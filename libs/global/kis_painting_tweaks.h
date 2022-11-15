7abcbaa21d377d46ff637167833173fceabc5219
Josh Soref <jsoref@gmail.com>
2022-11-15 10:08:24 -0500

    spelling: work around
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libs/global/kis_painting_tweaks.h
+++ libs/global/kis_painting_tweaks.h
@@ -26,7 +26,7 @@ namespace KisPaintingTweaks {
      * This is a workaround for QPainter::clipRegion() bug. When zoom
      * is about 2000% and rotation is in a range[-5;5] degrees, the
      * generated region will have about 20k+ rectangles inside. Their
-     * processing will be really slow. These functions fworkarounds
+     * processing will be really slow. These functions work around
      * the issue.
      */
     KRITAGLOBAL_EXPORT QRegion safeClipRegion(const QPainter &painter);
