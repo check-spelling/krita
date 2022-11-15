ce3b4176159dc1af06b1c7f571d23492c5a43b3e
Josh Soref <jsoref@gmail.com>
2022-11-15 10:10:26 -0500

    spelling: specified
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libs/libkis/Scratchpad.h
+++ libs/libkis/Scratchpad.h
@@ -34,7 +34,7 @@ public:
 public Q_SLOTS:
 
     /**
-     * @brief Clears out scratchpad with color specfified set during setup
+     * @brief Clears out scratchpad with color specified set during setup
      */
     void clear();
 
