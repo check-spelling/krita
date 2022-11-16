ce4db4f43e4be3934eed555a6c2856a606f1d38f
Josh Soref <jsoref@gmail.com>
2022-11-16 03:06:15 -0500

    spelling: request
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libs/image/kis_strokes_queue.cpp
+++ libs/image/kis_strokes_queue.cpp
@@ -88,7 +88,7 @@ struct Q_DECL_HIDDEN KisStrokesQueue::Private {
     KisLodSyncStrokeStrategyFactory lod0ToNStrokeStrategyFactory;
     KisSuspendResumeStrategyPairFactory suspendResumeUpdatesStrokeStrategyFactory;
     std::function<void()> purgeRedoStateCallback;
-    std::function<void()> postSyncLod0GUIPlaneRequistForResume;
+    std::function<void()> postSyncLod0GUIPlaneRequestForResume;
     KisSurrogateUndoStore lodNUndoStore;
     LodNUndoStrokesFacade lodNStrokesFacade;
     KisPostExecutionUndoAdapter lodNPostExecutionUndoAdapter;
@@ -717,7 +717,7 @@ void KisStrokesQueue::setPurgeRedoStateCallback(const std::function<void ()> &ca
 
 void KisStrokesQueue::setPostSyncLod0GUIPlaneRequestForResumeCallback(const std::function<void ()> &callback)
 {
-    m_d->postSyncLod0GUIPlaneRequistForResume = callback;
+    m_d->postSyncLod0GUIPlaneRequestForResume = callback;
 }
 
 KisPostExecutionUndoAdapter *KisStrokesQueue::lodNPostExecutionUndoAdapter() const
@@ -844,8 +844,8 @@ bool KisStrokesQueue::checkStrokeState(bool hasStrokeJobsRunning,
          * tiles into Lod0 mode.
          */
         if (needsSyncLod0PlaneToGUI &&
-            m_d->postSyncLod0GUIPlaneRequistForResume) {
-            m_d->postSyncLod0GUIPlaneRequistForResume();
+            m_d->postSyncLod0GUIPlaneRequestForResume) {
+            m_d->postSyncLod0GUIPlaneRequestForResume();
         }
 
         m_d->strokesQueue.dequeue(); // deleted by shared pointer
